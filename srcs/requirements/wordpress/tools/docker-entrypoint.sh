ce42acb5e227141e485a8a8748a9666a099e40b1
jgo-42 <jingmuio@gmail.com>
2023-08-25 06:17:17 +0000

    :pencil2: Fix(srcs): some typo

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -6,7 +6,7 @@ wp core download --path=/var/www/html
 
 wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb
 
-wp core install  --url=$WP_DOMAIN_NAME --title=$WP_DOMAIN_NAME --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_USER@42.fr
+wp core install  --url=$WP_DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_USER@42.fr
 wp user create $WP_NORM_USER $WP_NORM_USER@42.fr --role=author --user_pass=$WP_NORM_PASSWORD
 
 exec php-fpm81 -F -R
