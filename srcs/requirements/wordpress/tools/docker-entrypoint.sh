d29daaff4f7788ab998b9bbb5426f13335c370c8
jgo-42 <jingmuio@gmail.com>
2023-08-25 12:20:47 +0000

    :sparkles: Feat(redis): add redis container

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,4 +9,14 @@ wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$MARIADB_USER --dbpass=$MA
 wp core install  --url=$WP_DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_USER@42.fr
 wp user create $WP_NORM_USER $WP_NORM_USER@42.fr --role=author --user_pass=$WP_NORM_PASSWORD
 
+wp plugin install redis-cache --activate
+
+wp config set WP_REDIS_HOST redis
+wp config set WP_REDIS_PORT 6379
+wp config set WP_CACHE true
+
+wp redis enable
+
+chown -R www-data:www-data /var/www/html
+
 exec php-fpm81 -F -R
